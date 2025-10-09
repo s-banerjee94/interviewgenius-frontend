dac5738085d6b3df08458f69c8f529605bfe4461
s-banerjee94 <shondha.tara94@gmail.com>
2025-10-10 02:29:18 +0530

    Customize PrimeNG theme by defining a new preset with a semantic primary color palette.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,25 @@ import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import {definePreset} from '@primeuix/themes';
 
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{cyan.50}',
+      100: '{cyan.100}',
+      200: '{cyan.200}',
+      300: '{cyan.300}',
+      400: '{cyan.400}',
+      500: '{cyan.500}',
+      600: '{cyan.600}',
+      700: '{cyan.700}',
+      800: '{cyan.800}',
+      900: '{cyan.900}',
+      950: '{cyan.950}'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,12 +35,14 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
           prefix: 'ig',
           darkModeSelector: '.ig-dark',
+          cssLayer: false
         }
       }
     })
   ]
 };
+
